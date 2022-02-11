7175e005f13e195d586032657606b367ef6f1cfb
Saerom Bang <saerombang11@gmail.com>
2022-02-11 15:20:13 +0900

    Toggle Markup 추가

--- custom-component/src/App.js
+++ custom-component/src/App.js
@@ -1,12 +1,7 @@
 import Toggle from './component/Toggle';
 
 function App() {
-  return (
-    <>
-      <div className="bg-gray-800 text-3xl font-bold underline">Hello world!</div>
-      <Toggle />
-    </>
-  );
+  return <Toggle />;
 }
 
 export default App;
