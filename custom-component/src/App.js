245a2e0aa1a715f6ac94b6efa44dc8d32e3a693c
Saerom Bang <saerombang11@gmail.com>
2022-02-12 16:38:57 +0900

    Tab 마크업 추가

--- custom-component/src/App.js
+++ custom-component/src/App.js
@@ -1,10 +1,9 @@
-import Toggle from './component/Toggle';
+import Tab from './component/Tab';
 
 function App() {
   return (
     <>
-      <div className="bg-gray-800 text-3xl font-bold underline">Hello world!</div>
-      <Toggle />
+      <Tab />
     </>
   );
 }
