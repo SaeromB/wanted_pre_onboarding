07580c34a61aa51a6d4d702f4dbc1d4604e0400c
Saerom Bang <saerombang11@gmail.com>
2022-02-15 17:57:12 +0900

    AutoComplete 추가

--- custom-component/src/App.js
+++ custom-component/src/App.js
@@ -1,3 +1,4 @@
+import { AutoComplete } from './component/AutoComplete';
 import Toggle from './component/Toggle';
 
 function App() {
@@ -5,6 +6,7 @@ function App() {
     <>
       <div className="bg-gray-800 text-3xl font-bold underline">Hello world!</div>
       <Toggle />
+      <AutoComplete />
     </>
   );
 }
